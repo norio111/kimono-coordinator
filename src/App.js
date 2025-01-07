b82f836761a929f051c4a69744aedc9504cac272
nori3 <sindohnori@hotmail.com>
2025-01-07 22:16:33 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import KimonoCoordinator from './components/KimonoCoordinator';
+
+function App() {
+  return (
+    <div className="App">
+      <KimonoCoordinator />
+    </div>
+  );
+}
+
+export default App;
